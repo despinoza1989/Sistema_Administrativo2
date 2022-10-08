1f098ffb0f9cf1b217e7cdf356a72cd995e02201
despinoza1989 <73562537+despinoza1989@users.noreply.github.com>
2022-10-07 21:05:26 -0300

    modificaciones footer/carrusel/css footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -2,6 +2,7 @@
     <div class="marcas">
         <img src="assents/img/logo-mutual@2x.png" alt="mutual">
         <img src="assents/img/Logo_ISL_20cm.jpg" alt="isl">
+        <img src="assents/img/logo-ist.png" alt="ist">
         <img src="assents/img/logo-mundo_achs-negro.png" alt="achs">
     </div>
     <div class="logo_footer">
