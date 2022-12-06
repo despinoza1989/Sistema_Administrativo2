21bca9902d8c576d96abad8c6e490aea1459c458
luis matus <l.ignaciomatus@gmail.com>
2022-12-05 22:51:31 -0300

    update reportes globales

--- views/layout/reportes/reportes_globales.php
+++ views/layout/reportes/reportes_globales.php
@@ -2,10 +2,9 @@
     <div class="col-md-10" class="card">
         <div class="card border-secondary mb-3">
             <div class="card-header text-center">
-            <strong>Reportes y Estadísticas Globales</strong>
             </div>
-            <div class="card-body">
-            <iframe title="Reportes Sistema administrativo" width="1280" height="1024" src="https://app.powerbi.com/view?r=eyJrIjoiNGRhNjU4NjktMmEwMy00MzQyLTg2M2MtZmViMmMwYjAyODY3IiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>    
+            <div class="card-body">    
+            <iframe title="Reportes no mas accidentes Globales - Página 2" width="1280" height="1024" src="https://app.powerbi.com/view?r=eyJrIjoiYWJjZmUyMjktOWM1OC00NDlhLTg5Y2EtOGRiZTgxZGUyN2QxIiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
     </div>
