409588b35dc37969cde05d16b24881b8aa09bf34
luis matus <l.ignaciomatus@gmail.com>
2022-12-06 17:55:45 -0300

    upate reportes

--- views/layout/reportes/reportes_clientes.php
+++ views/layout/reportes/reportes_clientes.php
@@ -4,8 +4,30 @@
             <div class="card-header text-center">
             </div>
             <div class="card-body">
-            <iframe title="Reportes no mas accidentes Clientes - Página 1" width="1280" height="1024" src="https://app.powerbi.com/view?r=eyJrIjoiMmNiMjhmMzAtZmNmYy00MTk5LTlmMWYtNGZiOWE0OWNhYzJhIiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+                <iframe title="Reportes no mas accidentes Clientes" width="1280" height="1024"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiMmNiMjhmMzAtZmNmYy00MTk5LTlmMWYtNGZiOWE0OWNhYzJhIiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9&pageName=ReportSection"
+                    frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
     </div>
-</div>  
+    <div class="" align="center">
+        <button class="btn btn-primary col-10" onclick="openManualAdministrativo()">Descargar PDF</button>
+    </div>
+</div>
+
+<script>
+init()
+
+function openManualAdministrativo() {
+
+    window.open('http://localhost/Portafolio/sistema_administrativo2/assents/reportes/ReportesnomasaccidentesClientes.pdf');
+
+}
+
+
+function openManualProfesional() {
+
+    window.open('http://localhost/sistema_administrativo/assents/manual/ManualFerme.pdf');
+
+}
+</script>
--- views/layout/reportes/reportes_globales.php
+++ views/layout/reportes/reportes_globales.php
@@ -3,10 +3,31 @@
         <div class="card border-secondary mb-3">
             <div class="card-header text-center">
             </div>
-            <div class="card-body">    
-            <iframe title="Reportes no mas accidentes Globales - Página 2" width="1280" height="1024" src="https://app.powerbi.com/view?r=eyJrIjoiYWJjZmUyMjktOWM1OC00NDlhLTg5Y2EtOGRiZTgxZGUyN2QxIiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+            <div class="card-body">
+                <iframe title="Reportes no mas accidentes Globales" width="1280" height="1024"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiYWJjZmUyMjktOWM1OC00NDlhLTg5Y2EtOGRiZTgxZGUyN2QxIiwidCI6IjcyZmQwYjVhLThhNmEtNGNmZi04OWY2LWJkZTk2MWY3ZTI1MCIsImMiOjR9&pageName=ReportSection8d37594882ee86871883"
+                    frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
     </div>
-</div>  
+    <div class="" align="center">
+        <button class="btn btn-primary col-10" onclick="openManualAdministrativo()">Descargar PDF</button>
+    </div>
+</div>
 
+<script>
+init()
+
+function openManualAdministrativo() {
+
+    window.open('http://localhost/Portafolio/sistema_administrativo2/assents/reportes/ReportesnomasaccidentesGlobales.pdf');
+
+}
+
+
+function openManualProfesional() {
+
+    window.open('http://localhost/sistema_administrativo/assents/manual/ManualFerme.pdf');
+
+}
+</script>
